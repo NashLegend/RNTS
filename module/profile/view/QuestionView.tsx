0c4b4f64ab0292583536a79a96d239d4fc906539
nashlegend <panzhihuipzh@hotmail.com>
2017-02-09 18:58:39 +0800

    jsxelement

--- module/profile/view/QuestionView.tsx
+++ module/profile/view/QuestionView.tsx
@@ -5,7 +5,7 @@ import * as React from "react";
 import Answer from "../model/Answer";
 import {View} from "react-native";
 import Component = React.Component;
-import JSXElement = JSX.JSXElement;
+import JSXElement = JSX.Element;
 import Question from "../model/Question";
 export default class QuestionView extends Component<Question,any> {
 
